269c8b3011ef80c6bc447be90cdb14a9e3bdf2c2
kacper.sledz <casper.sledx@gmail.com>
2021-05-25 20:00:26 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { useEffect, useRef } from "react";
-import { Engine } from "./upstream/core";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const domElement = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (domElement.current) {
-      const engine = new Engine("1", {
-        container: { domElement: domElement.current },
-      });
-      engine.start();
     }
   }, [domElement]);
   return <div style={{ width: 700, height: 500 }} ref={domElement}></div>;
--- src/upstream/libs/shaders/clouds/vertex.glsl
+++
@@ -1,7 +0,0 @@
-varying vec3 eyenorm;
-
-void main() {
-	eyenorm = -normal;
-
-	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
